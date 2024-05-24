e5fce85bab6aca07e3e27f55461f830b746768d5
Saikat Mahmud <saikat8486@gmail.com>
2024-05-24 14:17:47 +0600

    service creation

---
+++ EcomMS.BLL/ServiceAccess/BusinessService.cs
@@ -0,0 +1,30 @@
+﻿using EcomMS.BLL.Services;
+using EcomMS.DAL.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcomMS.BLL.ServiceAccess
+{
+    public class BusinessService : IBusinessService
+    {
+        public CartService CartService { get; set; }
+        public CategoryService CategoryService { get; set; }
+        public CustomerService CustomerService { get; set; }
+        public EmployeeService EmployeeService { get; set; }
+        public OrderProductService OrderProductService { get; set; }
+        public OrderService OrderService { get; set; }
+        public OrderStatusHistoryService OrderStatusHistoryService { get; set; }
+        public ProductImageService ProductImageService { get; set; }
+        public ProductService ProductService { get; set; }
+        public ReviewService ReviewService { get; set; }
+        public SupplierProductService SupplierProductService { get; set; }
+        public SupplierService SupplierService { get; set; }
+        public BusinessService(IUnitOfWork unitOfWork)
+        {
+            
+        }
+    }
+}
---
+++ EcomMS.BLL/ServiceAccess/IBusinessService.cs
@@ -0,0 +1,25 @@
+﻿using EcomMS.BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcomMS.BLL.ServiceAccess
+{
+    public interface IBusinessService
+    {
+        public CartService CartService { get; set; }
+        public CategoryService CategoryService { get; set; }
+        public CustomerService CustomerService { get; set; }
+        public EmployeeService EmployeeService { get; set; }
+        public OrderProductService OrderProductService { get; set; }
+        public OrderService OrderService { get; set; }
+        public OrderStatusHistoryService OrderStatusHistoryService { get; set; }
+        public ProductImageService ProductImageService { get; set; }
+        public ProductService ProductService { get; set; }
+        public ReviewService ReviewService { get; set; }
+        public SupplierProductService SupplierProductService { get; set; }
+        public SupplierService SupplierService { get; set; }
+    }
+}
